9005e59d0ec3f52ea5fb5d6441e13bab09d91b6b
gugy <ggyrst@gmail.com>
2022-12-12 22:17:27 +0100

    kata SQL groupby exercise added

--- FIRSTWORD.sql
+++ FIRSTWORD.sql
@@ -1,35 +1,3 @@
-DB.create_table :monsters do
-  primary_key :id
-  String :name
-  Int :legs
-  Int :arms
-  String :characteristics
-end
-
-monsters = DB[:monsters] 
-
-
-monsters.insert(name: 'Cyril', legs: Faker::Address.building_number, arms:Faker::Address.building_number, characteristics:'big, smelly')
-monsters.insert(name: 'Tiny', legs: Faker::Address.building_number, arms:Faker::Address.building_number, characteristics:'small, stinky, loud')
-monsters.insert(name: 'Niall', legs: Faker::Address.building_number, arms:Faker::Address.building_number, characteristics:'flatulent')
-monsters.insert(name: 'Umph', legs: Faker::Address.building_number, arms:Faker::Address.building_number, characteristics:'idiotic, nasty')
-monsters.insert(name: 'Martin', legs: Faker::Address.building_number, arms:Faker::Address.building_number, characteristics:'mad, stupid, bad-breath')
-
-
-def expected
-  DB[%q(
-    SELECT id, name, 
-CASE 
-  WHEN position(','IN characteristics) >0 THEN LEFT(characteristics, position(',' IN characteristics)-1) 
-  ELSE characteristics 
-  END 
-  AS characteristic
-FROM monsters
-)].to_a
-  end
-
-
-
 --PRETTIER SOLUTION
 SELECT id,
        name, 
@@ -46,4 +14,4 @@ SELECT id, name,
   END
 AS characteristic
 FROM monsters
-ORDER BY id
+ORDER BY id;
